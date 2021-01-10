90f2b1cff886ca8092dbea2dcea2f9e355d40899
Azizjon <aziz89131983189@gmail.com>
2021-01-11 05:24:02 +0700

    modify my realesed

--- lessons3.py
+++ lessons3.py
@@ -3,6 +3,8 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import Perceptron
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
 import numpy as np
 
 # Loading Iris dataset
@@ -28,3 +30,40 @@ ppn.fit(X_train_std, y_train)
 
 y_pred = ppn.predict(X_test_std)
 print('Misclassified samples: %d' % (y_test != y_pred).sum())
+print( ' Strength: %.2f ' % accuracy_score(y_test , y_pred))
+
+def plot_decision_regions(X,y,classifier,test_idx=None,resolution=0.02):
+    #Setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+    
+    #Plot the decision surface
+    x1_min, x1_max = X[:,0].min()-1, X[:,0].max()+1
+    x2_min, x2_max = X[:,1].min()-1, X[:,1].max()+1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min,x1_max,resolution),np.arange(x2_min,x2_max,resolution))
+    Z = classifier.predict(np.array([xx1.ravel(),xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1,xx2,Z,alpha=0.4,cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(),xx2.max())
+    
+    #Plot class samples
+    for idx,cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.8,  c=cmap(idx), marker=markers[idx], label=cl)
+        
+    #Highlight test samples
+    if test_idx:
+        X_test,y_test = X[test_idx,:],y[test_idx]
+        plt.scatter(X_test[:,0],X_test[:,1],c='black',alpha=0.4,linewidths=1,marker='*',s=55,label='test set')
+            
+
+X_combined_std = np.vstack((X_train_std,X_test_std))
+y_combined = np.hstack((y_train,y_test))
+plot_decision_regions(X=X_combined_std,y=y_combined,classifier=ppn,test_idx=range(105,150))
+plt.xlabel('petal length [standarized]')
+plt.ylabel('petal width [standarized]')
+plt.legend(loc='upper left')
+plt.show()
+             
+  
